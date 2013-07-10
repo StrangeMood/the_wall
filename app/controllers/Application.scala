c5bbecd999670a7b753647c25f27589cea5baade
Ivan Efremov <st8998@gmail.com>
2013-07-10 12:16:47 +0400

    init

---
+++ app/controllers/Application.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("A lot of interesting will be here. Stay tuned..."))
+  }
+
+}
