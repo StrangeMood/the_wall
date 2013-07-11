e692c75080724ef6cfee630d3038e90c7f057f17
Ivan Efremov <st8998@gmail.com>
2013-07-11 22:18:31 +0400

    refactor github reads

--- app/controllers/GithubHook.scala
+++ app/controllers/GithubHook.scala
@@ -10,19 +10,20 @@ import models.Commit
 object GithubHook extends Controller {
 
   implicit val commitsRead = new Reads[List[Commit]] {
+    private val commitBuilder =
+      (__ \ "id").read[String] ~
+      (__ \ "message").read[String] ~
+      (__ \ "author").read(
+        (__ \ "email").read[String] ~
+        (__ \ "name").read[String]
+        tupled
+      ) ~
+      (__ \ "timestamp").read[String] ~
+      (__ \ "url").read[String]
+
     def reads(js: JsValue): JsResult[List[Commit]] = {
       (js \ "repository" \ "name").validate[String].flatMap { project =>
-        implicit val commitReads = (
-          (__ \ "id").read[String] ~
-          (__ \ "message").read[String] ~
-          (__ \ "author").read(
-            (__ \ "email").read[String] ~
-            (__ \ "name").read[String]
-            tupled
-          ) ~
-          (__ \ "timestamp").read[String] ~
-          (__ \ "url").read[String]
-        )(Commit.apply(_, _, _, _, _, project))
+        implicit val commitReads = commitBuilder(Commit.apply(_, _, _, _, _, project))
 
         (js \ "commits").validate[List[Commit]]
       }
