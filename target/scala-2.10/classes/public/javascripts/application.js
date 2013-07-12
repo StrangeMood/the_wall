b32ae4ca5e3cda731201e84ba1d8deabb01c6402
Ivan Efremov <st8998@gmail.com>
2013-07-12 19:22:49 +0400

    remove openshift crap

--- target/scala-2.10/classes/public/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-var ws = new WebSocket('ws://'+document.location.host+'/websocket')
-ws.onmessage = function(evt) {
-  console.log(evt.data)
-}
-
-//var source=new EventSource('/events');
-//source.onmessage = function(event) {
-//  console.log(event)
-//}
